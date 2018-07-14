8b9a5604ccbd7cace57fc3a9aae78b0c25a68507
Raymart De Guzman <tramyardg@gmail.com>
2018-07-14 04:27:40 -0400

    modular approach is cleaner

--- js/back2top.js
+++ js/back2top.js
@@ -1,56 +1,54 @@
-const Back2Top = {
-  id: null,
-  createBtn () {
-    let btn = document.createElement('button');
-    let btnId = 'back2topBtn';
-    let btnTxt = document.createTextNode('Top');
+const back2top = (function () {
+  return {
+    id: null,
+    setButtonId (buttonId) {
+      back2top.id = $('#' + buttonId)[0];
+    },
+    stylize () {
+      back2top.id.style.display = 'none';
+      back2top.id.style.position = 'fixed';
+      back2top.id.style.bottom = '20px';
+      back2top.id.style.right = '30px';
+      back2top.id.style.zIndex = '99';
+      back2top.id.style.fontSize = '18px';
+      back2top.id.style.color = 'white';
+      back2top.id.style.cursor = 'pointer';
+      back2top.id.style.padding = '15px';
+      back2top.id.style.borderRadius = '4px';
+    },
+    createButton () {
+      let btn = document.createElement('button');
+      let btnTxt = document.createTextNode('Top');
+      btn.appendChild(btnTxt);
 
-    btn.appendChild(btnTxt);
-    btn.setAttribute('title', 'Go to top');
-    btn.setAttribute('class', 'btn btn-primary');
-    btn.setAttribute('id', btnId);
-    document.body.appendChild(btn);
+      let btnId = 'back2topBtn';
+      btn.setAttribute('id', btnId);
+      btn.setAttribute('title', 'Go to top');
+      btn.setAttribute('class', 'btn btn-primary');
+      document.body.appendChild(btn);
 
-    Back2Top.setBtnId(btnId);
-    Back2Top.stylize();
-  },
-  setBtnId (btnId) {
-    Back2Top.id = $('#' + btnId)[0];
-  },
-  stylize () {
-    Back2Top.id.style.display = 'none';
-    Back2Top.id.style.position = 'fixed';
-    Back2Top.id.style.bottom = '20px';
-    Back2Top.id.style.right = '30px';
-    Back2Top.id.style.zIndex = '99';
-    Back2Top.id.style.fontSize = '18px';
-    Back2Top.id.style.color = 'white';
-    Back2Top.id.style.cursor = 'pointer';
-    Back2Top.id.style.padding = '15px';
-    Back2Top.id.style.borderRadius = '4px';
-  },
-  scrollFunction () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      Back2Top.id.style.display = 'block';
-    } else {
-      Back2Top.id.style.display = 'none';
+      back2top.setButtonId(btnId);
+      back2top.stylize();
+    },
+    scrollFunction () {
+      window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          back2top.id.style.display = 'block';
+        } else {
+          back2top.id.style.display = 'none';
+        }
+      };
+    },
+    topFunction () {
+      back2top.id.onclick = function () {
+        $('html, body').animate({scrollTop: 0}, 600);
+      };
+    },
+    run () {
+      back2top.createButton();
+      back2top.scrollFunction();
+      back2top.topFunction();
     }
-  },
-  topFunction () {
-    $(Back2Top.id).click(function (e) {
-      $('html, body').animate({scrollTop: 0}, 600);
-      e.preventDefault();
-      return false;
-    });
-  },
-  run () {
-    Back2Top.createBtn();
-    window.onscroll = function () {
-      Back2Top.scrollFunction();
-    };
-    Back2Top.topFunction();
-  }
-};
-(function () {
-  Back2Top.run();
+  };
 })();
+back2top.run();
